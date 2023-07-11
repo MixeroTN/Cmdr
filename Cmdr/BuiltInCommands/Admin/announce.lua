94697411271a1edf64a2a97ff58fb8682be154c9
mixeu <shiro@mailo.com>
2023-07-11 20:24:05 +0200

    Feature: announce command (popup)

--- Cmdr/BuiltInCommands/Admin/announce.lua
+++ Cmdr/BuiltInCommands/Admin/announce.lua
@@ -1,7 +1,7 @@
 return {
 	Name = "announce";
 	Aliases = {"m"};
-	Description = "Makes a server-wide announcement.";
+	Description = "Makes a server-wide announcement in a form of pop up.";
 	Group = "DefaultAdmin";
 	Args = {
 		{
--- Cmdr/BuiltInCommands/Admin/announceServer.lua
+++ Cmdr/BuiltInCommands/Admin/announceServer.lua
@@ -1,15 +1,22 @@
 local TextService = game:GetService("TextService")
-local Players = game:GetService("Players")
-local Chat = game:GetService("Chat")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local server = ServerScriptService:WaitForChild("Server")
+
+local preparationQueue = require(server:WaitForChild("PreparationQueue"))
+
+local indicators = preparationQueue:ReturnService("Indicators")
 
 return function (context, text)
+	-- // Filters the text
 	local filterResult = TextService:FilterStringAsync(text, context.Executor.UserId, Enum.TextFilterContext.PublicChat)
+	local resultForBroadcast = filterResult:GetNonChatStringForBroadcastAsync()
 
-	for _, player in ipairs(Players:GetPlayers()) do
-		if Chat:CanUsersChatAsync(context.Executor.UserId, player.UserId) then
-			context:SendEvent(player, "Message", filterResult:GetChatForUserAsync(player.UserId), context.Executor)
-		end
-	end
+	-- // Yield until Knit Framework is ready
+	preparationQueue:Await()
+
+	-- // Indicates the Popup, image id 0 will show no image
+	indicators:IndicatePopup("announcement", "rbxassetid://0", "[ANNOUNCEMENT] " .. resultForBroadcast, Color3.fromRGB(255, 238, 0), 5 + (#resultForBroadcast / 20))
 
 	return "Created announcement."
 end
